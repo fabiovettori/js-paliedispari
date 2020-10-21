cc31a3679931fbbb8f64d4f50e1e5672af0d567f
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-21 16:53:12 +0200

    v.01

--- palindroma/sandbox.js
+++ palindroma/sandbox.js
@@ -1,43 +1,65 @@
 // ESERCIZIO:
 // Palindroma: scrivere una funzione per valutare se una parola è palindroma oppure no (una parola è palindroma se la posso leggere nello stesso modo dall'inizio alla fine e viceversa, ad esempio "anna" è palindroma, "andrea" no)
 
-var userInput = 'I topi non avevano nipoti';
-var parolaDaAnalizzare = userInput.toLowerCase();
-// console.log(parolaDaAnalizzare);
-var parolaCampione = [];
+var userInput = prompt('Inserisci la parola/frase da analizzare');
+// var userInput = 'I topi non avevano nipoti';
 
-for (var i = 0; i < parolaDaAnalizzare.length; i++) {
-    var letteraDaAnalizzare = parolaDaAnalizzare[i];
-    if (letteraDaAnalizzare.charCodeAt() !== 32) {
-        parolaCampione.push(letteraDaAnalizzare);
+console.log(isPalindroma(userInput));
+
+// funzione che converte la parola in lettere minuscole ed elimina gli spazi tra le lettere
+function toLowerClean(parola){
+    var parolaCampione = [];
+    for (var i = 0; i < parola.length; i++) {
+        var letteraDaAnalizzare = parola[i];
+        // utilizzo il codice Unicode dello spazio per eliminarlo dalla parolaù7frase per eliminare
+        if (letteraDaAnalizzare.charCodeAt() !== 32) {
+            parolaCampione.push(letteraDaAnalizzare);
+        }
     }
-}
-console.log(parolaCampione);
-
-var parolaInvertita = [];
-for (var i = 0; i < parolaCampione.length; i++) {
-    var letteraDaAnalizzare = parolaCampione[i];
-    parolaInvertita.unshift(parolaCampione[i]);
+    return parolaCampione;
+    // console.log(parolaCampione);
 }
 
-console.log(parolaInvertita);
 
-for (var i = 0; i < parolaCampione.length; i++) {
-    var check = false;
-    if (parolaCampione[i] !== parolaInvertita[i]) {
-        check = true;
+// funzione che capovolge l'ordine della parola
+function toInvert(parola){
+    var parolaInvertita = [];
+    for (var i = 0; i < parola.length; i++) {
+        var letteraDaAnalizzare = parola[i];
+        // spingo ciascuna lettera della parola/frase in prima posizione di un nuovo array cosi da capovolgerne la posizione finale
+        parolaInvertita.unshift(parola[i]);
     }
-} if (!check) {
-    console.log('La frase/parola scelta è palindroma');
-} else {
-    console.log('La frase/parola scelta non è palindroma');
+    return parolaInvertita;
+    // console.log(parolaInvertita);
 }
 
-// test1
+
+// funziona che controlla se la frase/parola è palindroma
+function isPalindroma(parola){
+    for (var i = 0; i < parola.length; i++) {
+        var parolaCampione = toLowerClean(parola);
+        var parolaInvertita = toLowerClean(toInvert(parola));
+        var check = false;
+        if (parolaCampione[i] !== parolaInvertita[i]) {
+            check = true;
+        }
+    var output;
+    } if (!check) {
+        output = 'La frase/parola scelta è palindroma';
+        // console.log(output);
+    } else {
+        output = 'La frase/parola scelta non è palindroma';
+        // console.log(output);
+    }
+    return output;
+}
+
+
+// test1 - determinare Unicode dello spazio
 // var test = ' ';
 // console.log(test.charCodeAt());
 
-// test2
+// test2 - verifico la funzione unshift()
 // var test = [1, 2, 3];
-// var pop = test.pop();
+// var pop = test.unshift();
 // console.log(pop);
